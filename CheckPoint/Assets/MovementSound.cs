baae42ef1dd6a6d62e57637a18a7fdab57ff0039
Charles Meeler <cmeeler@usc.edu>
2024-04-23 20:12:16 -0700

    player movement sounds

---
+++ CheckPoint/Assets/MovementSound.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementSound : MonoBehaviour
+{
+    public PlayerController onTheGround;
+
+    public AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onTheGround.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                audioSrc.Play();
+            }
+        }
+
+        if (!onTheGround.isGrounded || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            audioSrc.Stop();
+        }
+    }
+}
